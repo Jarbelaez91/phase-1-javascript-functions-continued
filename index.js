0d0770fa17a41c86c8d528bbdb16fa254a863202
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-02-24 17:28:17 -0500

    completed assignemt

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun (want = "roller-skate") {
+    return (`This Saturday, I want to ${want}!`);
+}
+console.log(saturdayFun("bathe my dog"))
+console.log(saturdayFun)
+
+function mondayWork(monday = "go to the office") {
+    return (`This Monday, I will ${monday}.`);
+}
+console.log(mondayWork("work from home"))
+console.log(mondayWork)
+
+function wrapAdjective(flair ='*', adj){
+    let wrappedSentence = function (adj = 'special'){
+        return `You are ${flair}${adj}${flair}!`
+    }
+    return wrappedSentence;
+    }
+console.log(`*`, "a hard worker")
+console.log(`||`,"a dedicated programmer")
+console.log()
