2670418da5c1ee85fa8a7614c598f0ae9e55fe7b
Jeffrey Arbelaez <Jeffreyarbelaez@yahoo.com>
2023-05-09 17:55:33 -0500

    completed assignemnt

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-// code your solution here
-function saturdayFun (want = "roller-skate") {
-    return (`This Saturday, I want to ${want}!`);
+function saturdayFun (saturday = 'roller-skate') {
+    return `This Saturday, I want to ${saturday}!`
 }
-console.log(saturdayFun("bathe my dog"))
-console.log(saturdayFun)
 
-function mondayWork(monday = "go to the office") {
-    return (`This Monday, I will ${monday}.`);
+
+function mondayWork(monday = 'go to the office') {
+    return `This Monday, I will ${monday}.`
 }
-console.log(mondayWork("work from home"))
-console.log(mondayWork)
 
-function wrapAdjective(flair ='*', adj){
-    let wrappedSentence = function (adj = 'special'){
-        return `You are ${flair}${adj}${flair}!`
-    }
-    return wrappedSentence;
-    }
-console.log(`*`, "a hard worker")
-console.log(`||`,"a dedicated programmer")
-console.log()
+function wrapAdjective (string = '*') {
+    return function (parameter = "a hard worker") {
+    return `You are ${string}${parameter}${string}!`
+}
+}
